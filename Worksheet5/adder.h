97234e35068090775ead2ed4f79d0213bb408869
Aaron Camo <eeyac11@nottingham.ac.uk>
2026-02-12 15:48:24 +0000

    Implement Worksheet5

---
+++ Worksheet5/adder.h
@@ -0,0 +1,30 @@
+//begin adder.h
+/** @file
+* This file contains declarations of all exported functions in the maths library.
+*/
+
+/** Adding function
+* This function adds two numbers
+* @param a is the first number
+* param b is the second number
+* @return sum of a and b
+*/
+#ifndef MATHSLIB_ADDER_H
+	#define MATHSLIB_ADDER_H
+	#if defined(WIN32)|defined(_WIN32)
+		#ifdef maths_STATIC
+			#define MATHSLIB_API
+		#else
+			#ifdef maths_EXPORTS
+				#define MATHSLIB_API __declspec(dllexport)
+			#else
+				#define MATHSLIB_API __declspec(dllimport)
+			#endif
+		#endif
+	#else
+		#define MATHSLIB_API
+	#endif
+	MATHSLIB_API int add(int a, int b);
+#endif
+//end adder.h
+	
